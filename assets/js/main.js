fc106e4374cf0483a4b084fad9c13970a7e27744
DaniloSalerno <salerno.danilo@hotmail.it>
2023-08-24 16:28:32 +0200

    add validator for km input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,56 @@
 const btn = document.querySelector('[type=submit]');
 
 btn.addEventListener('click', (e) => {
+
     e.preventDefault();
+    
     let userName = document.querySelector('#name').value;
     let kmUserWant = document.querySelector('#km').value;
     let userAge = document.querySelector('#age').value;
-    console.log(userName,kmUserWant, userAge);
+    
+    if (isNaN(kmUserWant)) {
 
-const ticketPrice = kmUserWant * 0.21;
+    alert('non hai inserito un numero nella richiesta dei chilometri che vuoi percorrere,per favore inserisci un numero valido');
+    window.location.reload();
 
-let ticketRebate;
-let finalPrice;
+    } else if (kmUserWant < 1 || kmUserWant > 1500) {
 
-if (userAge === 'junior') {
+    alert('non hai inserito un numero valido nella richiesta dei chilometri che vuoi percorrere,per favore inserisci un numero compreso tra 1 e 1500');
+    window.location.reload();
 
-    ticketRebate = ticketPrice * 20 / 100;
-    finalPrice =(ticketPrice - ticketRebate).toFixed(2);
-    console.log(finalPrice);
+    } 
 
-} else if (userAge === 'senior') {
+    const ticketPrice = kmUserWant * 0.21;
 
-    ticketRebate = ticketPrice * 40 / 100;
-    finalPrice = (ticketPrice - ticketRebate).toFixed(2);
-    console.log(finalPrice);
+    let ticketRebate;
+    let finalPrice;
 
-} else {
+    if (userAge === 'junior') {
 
-    finalPrice = ticketPrice.toFixed(2);
-    console.log(finalPrice);
+        ticketRebate = ticketPrice * 20 / 100;
+        finalPrice =(ticketPrice - ticketRebate).toFixed(2);
+        console.log(finalPrice);
 
-};
+    } else if (userAge === 'senior') {
+
+        ticketRebate = ticketPrice * 40 / 100;
+        finalPrice = (ticketPrice - ticketRebate).toFixed(2);
+        console.log(finalPrice);
+
+    } else {
+
+        finalPrice = ticketPrice.toFixed(2);
+        console.log(finalPrice);
+
+    };
 
 
-const userNameTicket = document.querySelector('.user_name');
-userNameTicket.textContent = userName;
+    const userNameTicket = document.querySelector('.user_name');
+    userNameTicket.textContent = userName;
 
-const userTicketPrice = document.querySelector('.ticket_price');
-userTicketPrice.textContent = finalPrice + ' €';
+    const userTicketPrice = document.querySelector('.ticket_price');
+    userTicketPrice.textContent = finalPrice + ' €';
 })
-
-/* btn.addEventListener('click', (e) => {
-    e.preventDefault();
-}) */
 
 /* if (isNaN(kmUserWant)) {
 
